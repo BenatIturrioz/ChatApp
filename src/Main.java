ad586ec49e86fc4362495ef410bdb87c83dafd5e
BenatIturrioz <beitumu7@gmail.com>
2025-01-07 16:12:14 +0100

    zerbitariarentzarako chat aplikazioa

---
+++ src/Main.java
@@ -0,0 +1,25 @@
+import java.io.IOException;
+import java.net.Socket;
+
+public class Main {
+    public Main(){
+    }
+
+    public static void main(String[] args){
+        Zerbitzaria zerbitzaria = new Zerbitzaria(5555);
+
+        try{
+            zerbitzaria.hasi();
+
+            while(zerbitzaria.konektatuta()){
+                Socket bezeroarenSocketa = zerbitzaria.onartuKonexioa();
+                Bezeroa bezero = new Bezeroa(bezeroarenSocketa);
+                zerbitzaria.geituBezeroa(bezero);
+                BezeroenKonexioa konexioa = new BezeroenKonexioa(bezero, zerbitzaria);
+                konexioa.start();
+            }
+        } catch (Exception ioe) {
+            ioe.printStackTrace();
+        }
+    }
+}
