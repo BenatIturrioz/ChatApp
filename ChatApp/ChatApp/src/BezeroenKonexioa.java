468705a7a1027518755252c2d3cc5b13d9b18b6f
BenatIturrioz <beitumu7@gmail.com>
2025-01-07 16:17:34 +0100

    Add files via upload

---
+++ ChatApp/ChatApp/src/BezeroenKonexioa.java
@@ -0,0 +1,26 @@
+import java.io.IOException;
+
+public class BezeroenKonexioa extends Thread{
+
+    private Bezeroa bezero;
+    private Zerbitzaria zerbitzaria;
+
+    public BezeroenKonexioa(Bezeroa bezero, Zerbitzaria zerbitzaria) {
+        this.bezero = bezero;
+        this.zerbitzaria = zerbitzaria;
+    }
+
+    public void run() {
+        this.bezero.out.println("Kaixo ongi etorri gure mezularitza zerbitzura!");
+
+        try {
+            while (this.bezero.konektatutaDago()) {
+                String mezua = this.bezero.in.readLine();
+                // Pasa el cliente actual como remitente
+                this.zerbitzaria.bidaliMezuaDenei(mezua, this.bezero);
+            }
+        } catch (IOException ioe) {
+            ioe.printStackTrace();
+        }
+    }
+}
---
+++ ChatApp/ChatApp/src/Zerbitzaria.java
@@ -0,0 +1,40 @@
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
+
+public class Zerbitzaria {
+    private ServerSocket socket;
+    private int puerto;
+    private ArrayList<Bezeroa> bezeroak;
+
+    public Zerbitzaria(int puerto) {
+        this.puerto = puerto;
+        this.bezeroak = new ArrayList<>();
+    }
+
+    public void hasi() throws IOException {
+        this.socket = new ServerSocket(this.puerto);
+    }
+
+    public void itxi() throws IOException{
+        this.socket.close();
+    }
+
+    public boolean konektatuta(){return !this.socket.isClosed();}
+
+    public Socket onartuKonexioa() throws IOException{
+        return this.socket.accept();
+    }
+
+    public void geituBezeroa(Bezeroa bezero){this.bezeroak.add(bezero);}
+
+    public void bidaliMezuaDenei(String mezua, Bezeroa bidaltzailea) {
+        for (Bezeroa bezeroa : this.bezeroak) {
+            // No enviar el mensaje al remitente
+            if (bezeroa != bidaltzailea) {
+                bezeroa.out.println(mezua);
+            }
+        }
+    }
+}
